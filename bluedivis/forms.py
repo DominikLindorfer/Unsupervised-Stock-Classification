100a971021dfffed8c71c11fb503fb8f503b7d83
Dr. Dominik Lindorfer <dominik.lindorfer@jku.at>
2022-04-13 14:40:16 +0200

    Add files via upload

---
+++ bluedivis/forms.py
@@ -0,0 +1,27 @@
+# dwitter/forms.py
+
+from django import forms
+from .models import Dweet
+
+# class DweetForm(forms.ModelForm):
+#     body = forms.CharField(required=True)
+
+#     class Meta:
+#         model = Dweet
+#         exclude = ("user", )
+
+class DweetForm(forms.ModelForm):
+    body = forms.CharField(
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                "placeholder": "Enter a TICKER...",
+                "class": "input is-link is-large",
+            }
+        ),
+        label="",
+    )
+
+    class Meta:
+        model = Dweet
+        exclude = ("user", )
